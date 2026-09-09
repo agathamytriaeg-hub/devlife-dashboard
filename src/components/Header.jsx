8d3b35267062de32a477639d0c7f4458a3b20f44
Agatha <agathamytriaeg@gmail.com>
2026-09-09 15:59:23 -0300

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
+import {useState} from "react";
+import Relogio from "./Relogio";
+
 function Header(){
+    const [mostrarRelogio, setMostrarRelogio] = useState(true);
     return(
         <header className="bg-slate-900 text-white px-8 py-4">
             <h1>Devlife Dashboard</h1>
+
+            <div className="flex items-center gap-3">
+                {}
+                {mostrarRelogio && <Relogio />}
+
+                <button
+                onClick={() => setMostrarRelogio(!mostrarRelogio)}
+                >
+                    {mostrarRelogio ? "Esconder relógio":"Mostrar relógio"}
+                </button>
+            </div>
         </header>
     );
 }
